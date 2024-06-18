9eecbf1d88fcfe3122b94aae367278e5eccff435
tobieabel <tobieabel@gmail.com>
2024-06-18 11:20:14 +0100

    Updated with FullScreenWindow class

--- GUI/main_window.py
+++ GUI/main_window.py
@@ -1,6 +1,8 @@
-from PyQt5.QtWidgets import QMainWindow, QPushButton, QVBoxLayout, QWidget, QLabel, QVBoxLayout
-from PyQt5.QtGui import QImage, QPixmap
-from PyQt5.QtCore import Qt, QSize
+from PyQt5.QtWidgets import QMainWindow, QPushButton, QHBoxLayout, QWidget, QLabel, QVBoxLayout, QStackedLayout, QApplication
+from PyQt5.QtGui import QImage, QPixmap, QFont
+from PyQt5.QtCore import Qt, QSize, pyqtSignal, pyqtSlot, QUrl
+from PyQt5.QtMultimedia import QMediaPlayer, QMediaContent
+from PyQt5.QtMultimediaWidgets import QVideoWidget
 
 import Model.object_detection
 from Model.object_detection import video_update
@@ -17,16 +19,29 @@ class MainWindow(QMainWindow):
         self.start_button.setFixedSize(QSize(100, 50))
         self.stop_button = QPushButton('Stop')
         self.stop_button.setFixedSize(QSize(100, 50))
+        self.puzzle_button = QPushButton('Play Puzzle')
+        self.puzzle_button.setFixedSize(QSize(100, 50))
         self.video_widget = VideoWidget()
+        self.fullscreen_window = None
 
+        # Create a horizontal layout for buttons
+        button_layout = QHBoxLayout()
+        button_layout.addWidget(self.start_button)
+        button_layout.addWidget(self.stop_button)
+        button_layout.addWidget(self.puzzle_button)
+
+        # Create a vertical layout and add the button layout and video widget
         layout = QVBoxLayout()
-        layout.addWidget(self.start_button)
-        layout.addWidget(self.stop_button)
+        layout.addLayout(button_layout)  # Add the horizontal button layout
         layout.addWidget(self.video_widget)
 
         container = QWidget()
         container.setLayout(layout)
         self.setCentralWidget(container)
+
+    def create_fullscreen_window(self):
+        self.fullscreen_window = FullScreenWindow()
+
 
 
 
@@ -77,4 +92,97 @@ class VideoWidget(QWidget):
     def disconnect_signal(self):
         video_update.frame_ready.disconnect(self.connection_frame_ready)  # Disconnect the signal
         video_update.terminate_pipeline.disconnect(self.connection_terminate_pipeline)  # Disconnect the signal
+
+#widget for displaying a seperate screeen on a given monitor for the puzzle
+class FullScreenWindow(QWidget):
+    frame_received = pyqtSignal(QImage)
+    play_video_signal = pyqtSignal(str)
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle('Full Screen Window')
+        self.setWindowFlag(Qt.FramelessWindowHint)  # No border
+
+        self.stacked_layout = QStackedLayout()
+
+        # Camera frame layout
+        self.camera_frame_label = QLabel(self)
+        self.camera_frame_label.setAlignment(Qt.AlignCenter)
+        self.stacked_layout.addWidget(self.camera_frame_label)
+
+        # Black screen with puzzle_text layout
+        self.clue_widget = QLabel('This is a clue to the puzzle', self)
+        self.clue_widget.setAlignment(Qt.AlignCenter)
+        self.clue_widget.setStyleSheet("background-color: black; color: gold;")
+        font = QFont("Arial", 48, QFont.Bold)
+        self.clue_widget.setFont(font)
+        self.stacked_layout.addWidget(self.clue_widget)
+
+        # Black screen without text layout
+        self.black_screen_widget = QWidget(self)
+        self.black_screen_widget.setStyleSheet("background-color: black;")
+        self.stacked_layout.addWidget(self.black_screen_widget)
+
+        self.setLayout(self.stacked_layout)
+
+        # Media player setup
+        self.media_player = QMediaPlayer(None, QMediaPlayer.VideoSurface)
+        self.video_widget = QVideoWidget(self)
+        self.media_player.setVideoOutput(self.video_widget)
+        self.media_player.mediaStatusChanged.connect(self.on_media_status_changed)
+
+    def move_to_monitor(self, monitor_index):
+        screen = QApplication.screens()[monitor_index]
+        self.move(screen.geometry().x(), screen.geometry().y())
+        self.resize(screen.size())
+
+
+    def show_camera_frame(self):
+        self.camera = cv2.VideoCapture(0)  # Use the first camera
+        self.stacked_layout.setCurrentIndex(0)
+
+
+    def show_clue_screen(self):
+        self.stacked_layout.setCurrentIndex(1)
+
+    def show_black_screen(self):
+        self.stacked_layout.setCurrentIndex(2)
+
+    def play_mp4_video(self, video_path):
+        """
+        Play the specified MP4 video in full screen mode and close the window when done.
+        """
+        self.setLayout(QVBoxLayout())
+        self.layout().addWidget(self.video_widget)
+
+        video_url = QUrl.fromLocalFile(video_path)
+        self.media_player.setMedia(QMediaContent(video_url))
+        self.showFullScreen()
+        self.media_player.play()
+
+    def on_media_status_changed(self, status):
+        """
+        Handle changes in media status to close the window when the video finishes.
+        """
+        if status == QMediaPlayer.EndOfMedia:
+            self.close_FullScreenWindow()
+
+    def close_FullScreenWindow(self):
+        self.close()  # Close the window completely
+        self.deleteLater()  # Schedule the QWidget for deletion
+
+
+    def closeEvent(self, event):
+        """
+        Handles the close event to ensure camera is released properly.
+        """
+        print("closeEvent called")  # Debugging output
+        self.mdeia_player.stop()  # Stop the media player
+        event.accept()
+
+
+
 
--- controller/Main_controller.py
+++ controller/Main_controller.py
@@ -1,14 +1,29 @@
 import threading
 import Model.object_detection as od
 import time
+
+
 class MainController:
+
     def __init__(self, main_window):
         self.main_window = main_window
         self.main_window.start_button.clicked.connect(self.start_application)
         self.main_window.stop_button.clicked.connect(self.stop_application)
+        self.main_window.puzzle_button.clicked.connect(self.start_puzzle)
         self.thread = None
 
     def start_application(self):
+
+        if self.main_window.fullscreen_window !=None:
+            self.main_window.fullscreen_window.show_black_screen()
+            self.main_window.fullscreen_window.move_to_monitor(1)  # monitor 2 would be (index 1)
+            self.main_window.fullscreen_window.showFullScreen()
+        else:
+            self.main_window.create_fullscreen_window()
+            self.main_window.fullscreen_window.show_black_screen()
+            self.main_window.fullscreen_window.move_to_monitor(1)  # monitor 2 would be (index 1)
+            self.main_window.fullscreen_window.showFullScreen()
+
         if self.thread is None or not self.thread.is_alive():
             self.main_window.video_widget.connect_signal() #connection to slot between video widget and object detection script
             od.video_update.terminate_pipeline.connect(self.Stop_Inference_pipeline) #connection between object detection script and main controller
@@ -17,8 +32,8 @@ class MainController:
 
 
     def Stop_Inference_pipeline(self):
-        od.end_pipeline()
 
+        od.end_pipeline()
         self.thread.join()
         self.thread = None
         # self.main_window.video_widget.disconnect_signal() #disconnect signal between front and back ends, think this is not needed to close threads
@@ -30,6 +45,9 @@ class MainController:
 
     def stop_application(self):
         # Code to stop the application
+        if self.main_window.fullscreen_window != None:
+            self.main_window.fullscreen_window.close_FullScreenWindow()
+            self.main_window.fullscreen_window = None
         if self.thread is not None:
             #self.main_window.video_widget.disconnect_signal() #disconnect signal between front and back ends, think this is not needed to close threads
             od.end_pipeline()
@@ -40,4 +58,17 @@ class MainController:
                 time.sleep(0.1)
             print("threads terminated")
             self.main_window.video_widget.black_frame()
+
+    def start_puzzle(self):
+        if self.main_window.fullscreen_window !=None:
+            self.main_window.fullscreen_window.show_clue_screen()
+            self.main_window.fullscreen_window.move_to_monitor(1)  # monitor 2 would be (index 1)
+            self.main_window.fullscreen_window.showFullScreen()
+        else:
+            self.main_window.create_fullscreen_window()
+            self.main_window.fullscreen_window.show_clue_screen()
+            self.main_window.fullscreen_window.move_to_monitor(1)  # monitor 2 would be (index 1)
+            self.main_window.fullscreen_window.showFullScreen()
+
+
 
